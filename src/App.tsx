b04aeac964c5834645c7fff7a08f4c8c108af397
sunnapuvinay <138471157+sunnapuvinay@users.noreply.github.com>
2025-11-17 14:01:17 +0530

    added companies data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 const App = () =>{
+
+useEffect(() => {
+    axios.get('/companies.json')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
+
   return (
     <div>
       <h1 className="text-red-400 text-2xl font-extrabold">Frontlines Media</h1>
